5568b83bed51dafe2726bbac176b341ec9acdd91
JaniITmer <batyus4563@gmail.com>
2025-03-21 13:17:10 +0100

    minden ertesites blokkolasa kiveve ami allowedapp

--- app/src/main/java/com/janos/nagy/ezlearnapp/MyNotificationService.java
+++ app/src/main/java/com/janos/nagy/ezlearnapp/MyNotificationService.java
@@ -4,19 +4,34 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MyNotificationService extends NotificationListenerService {
     private static final String TAG = "MyNotificationService";
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        // Új értesítés érkezett
         String packageName = sbn.getPackageName();
-        Log.i(TAG, "Értesítés érkezett: " + packageName);
 
+        // saját alkalmazás csomagneve:
+        String myAppPackageName = "com.janos.nagy.ezlearnapp";
 
-        if ("com.facebook.katana".equals(packageName) || "com.instagram.android".equals(packageName)) {
-            cancelNotification(sbn.getKey()); // Értesítés eltávolítása
+        // megengedett alkalmazások
+        List<String> allowedApps = Arrays.asList(
+                myAppPackageName,           //  EzLearnApp
+                "com.android.systemui",     // Rendszer értesítések
+                "com.google.android.gm"
+        );
+
+        // Ha az alkalmazás nincs a megengedett listán, töröld az értesítést
+        if (!allowedApps.contains(packageName)) {
+            cancelNotification(sbn.getKey());
         }
+
+        // Naplózás (opcionális)
+        Log.i("NotificationService", "Értesítés érkezett: " + packageName + " - " +
+                (allowedApps.contains(packageName) ? "Megengedett" : "Törölve"));
     }
 
     @Override
